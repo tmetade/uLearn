0413de48042749baef6a9ca5e3b60dbf0eecfa47
tmetade <tmetade@yahoo.ca>
2018-03-20 15:49:44 -0400

    added card matching game json

--- app/src/main/java/com/example/tmetade/ulearn/games/CardMatching.java
+++ app/src/main/java/com/example/tmetade/ulearn/games/CardMatching.java
@@ -3,6 +3,7 @@ package com.example.tmetade.ulearn.games;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,17 +16,31 @@ import android.widget.ImageView;
 
 import com.example.tmetade.ulearn.R;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class CardMatching extends Fragment implements View.OnClickListener
 {
+    private static final String TAG = "CardMatching";
 
     private ImageView mCardView;
+    JSONObject mJson;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        try
+        {
+            mJson = new JSONObject(readJSONFromAsset());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
         return inflater.inflate(R.layout.fragment_card_matching, container, false);
-
-
     }
 
 
@@ -67,5 +82,26 @@ public class CardMatching extends Fragment implements View.OnClickListener
     public void onClick(View v)
     {
         flip(v.getId());
+    }
+
+
+    public String readJSONFromAsset()
+    {
+        String json = null;
+        try
+        {
+            AssetManager assetManager = getActivity().getBaseContext().getAssets();
+            InputStream is = assetManager.open("card_game.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
